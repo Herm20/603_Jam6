88e7dc37ef63ffc870d116004b2a404298b4c0cb
Haoyu Guo (RIT Student) <hg7364@ad.rit.edu>
2019-03-04 15:30:06 -0500

    finish grid

--- 603_Jam6/Assets/Scripts/PlayerCombat.cs
+++ 603_Jam6/Assets/Scripts/PlayerCombat.cs
@@ -7,10 +7,14 @@ public class PlayerCombat : MonoBehaviour
     private int lastMatchedBeat;
     private Vector3 speed;
     private bool freeze;
+    [SerializeField] private Grid grid;
+    [SerializeField] private GameObject modelHandler;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         freeze = false;
+        anim = modelHandler.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,44 +30,63 @@ public class PlayerCombat : MonoBehaviour
             {
                 // ok for move Horizontal
                 lastMatchedBeat = Beats._instance.counter;
-                Vector3 target = new Vector3(transform.position.x + -1 * 1.145f, transform.position.y, transform.position.z);
-                StartCoroutine(movement(target));
+                moveTo(-1, 0);
+                // Vector3 target = new Vector3(transform.position.x + -1 * 1.145f, transform.position.y, transform.position.z);
+                // StartCoroutine(movement(target));
             }
 
             if (Input.GetButtonDown("Right") && Beats._instance.inBeat && lastMatchedBeat != Beats._instance.counter)
             {
                 // ok for move Horizontal
                 lastMatchedBeat = Beats._instance.counter;
-                Vector3 target = new Vector3(transform.position.x + 1 * 1.145f, transform.position.y, transform.position.z);
-                StartCoroutine(movement(target));
+                // Vector3 target = new Vector3(transform.position.x + 1 * 1.145f, transform.position.y, transform.position.z);
+                // StartCoroutine(movement(target));
+                moveTo(1, 0);
             }
 
             if (Input.GetButtonDown("Up") && Beats._instance.inBeat && lastMatchedBeat != Beats._instance.counter)
             {
                 // ok for move Vertical
                 lastMatchedBeat = Beats._instance.counter;
-                Vector3 target = new Vector3(transform.position.x, transform.position.y + 1 * 0.9f, transform.position.z);
-                StartCoroutine(movement(target));
+                // Vector3 target = new Vector3(transform.position.x, transform.position.y + 1 * 0.9f, transform.position.z);
+                // StartCoroutine(movement(target));
+                moveTo(0, 1);
             }
 
             if (Input.GetButtonDown("Down") && Beats._instance.inBeat && lastMatchedBeat != Beats._instance.counter)
             {
                 // ok for move Vertical
                 lastMatchedBeat = Beats._instance.counter;
-                Vector3 target = new Vector3(transform.position.x, transform.position.y + -1 * 0.9f, transform.position.z);
-                StartCoroutine(movement(target));
+                moveTo(0, -1);
+                // Vector3 target = new Vector3(transform.position.x, transform.position.y + -1 * 0.9f, transform.position.z);
+
             }
         }
 
-
+    }
+    private void moveTo(int xDiff, int yDiff){
+        Vector3Int cellPosition = grid.WorldToCell(transform.position);
+        print("current position in grid: " + cellPosition);
+        cellPosition += new Vector3Int(xDiff, yDiff, 0);
+        var newPos = grid.GetCellCenterWorld(cellPosition);
+        print("old pos: " + transform.position);
+        print("new pos: " + newPos);
+        StartCoroutine(movement(newPos));
+    }
+    private void OnDrawGizmos() {
+        Vector3Int cellPosition = grid.WorldToCell(transform.position);
+        var newPos = grid.GetCellCenterWorld(cellPosition);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(newPos, 0.2f);
     }
 
     IEnumerator movement(Vector3 target) {
         freeze = true;
+        anim.Play("Interacting", -1, 0);
         int counter = 0;
         while (counter < 5) {
-            var x  = Mathf.SmoothDamp(transform.position.x,target.x,ref speed.x, 0.05f);
-            var y = Mathf.SmoothDamp(transform.position.y, target.y, ref speed.y, 0.05f);
+            var x  = Mathf.SmoothDamp(transform.position.x,target.x,ref speed.x, 0.03f);
+            var y = Mathf.SmoothDamp(transform.position.y, target.y, ref speed.y, 0.03f);
             transform.position = new Vector3(x, y, transform.position.z);
             yield return new WaitForSeconds(0.01f);
             counter++;
