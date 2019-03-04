1611912c4b9eb837f185cfc44293532ceedd5d78
“Freddie <masterfreddiewu@gmail.com>
2019-03-04 17:07:18 -0500

    Basic enemy movement added.

---
+++ 603_Jam6/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float speed;
+    private WayPoints wayPoints;
+
+    private int waypointIndex = 0;
+
+    void Start() {
+       wayPoints = GameObject.FindGameObjectWithTag("WayPoints").GetComponent<WayPoints>(); 
+    }    
+
+    void Update(){
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, wayPoints.waypoints[waypointIndex].position) < 0.1f){
+            if(waypointIndex < wayPoints.waypoints.Length - 1) {
+                waypointIndex++;
+                Vector3 direction = wayPoints.waypoints[waypointIndex].position - transform.position;
+                float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+                angle = Mathf.Abs(angle);
+                angle += 90;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+            else Destroy(gameObject);
+        } 
+    }
+}
